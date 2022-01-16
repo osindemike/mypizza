ffcdf4b7338873ef3c8485936d33b90d707a2ba4
OsindeMike <michael.osinde@student.moringaschool.com>
2022-01-16 17:53:24 +0300

    add menu hover

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// business logic
+var price;
+function pizza(size, crust, toppings, amount, totalprice){
+this.size=size;
+this.crust=crust;
+this.toppings=toppings;
+this.amount=amount;
+this.totalprice=totalprice;
+}
+$(document).ready(function(){
+  $("#pizza1").mouseover(function(){
+    $("#slide1").show();
+  }).mouseout(function(){
+    $("#slide1").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza2").mouseover(function(){
+    $("#slide2").show();
+  }).mouseout(function(){
+    $("#slide2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza3").mouseover(function(){
+    $("#slide3").show();
+  }).mouseout(function(){
+    $("#slide3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza4").mouseover(function(){
+    $("#slide4").show();
+  }).mouseout(function(){
+    $("#slide4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza5").mouseover(function(){
+    $("#slide5").show();
+  }).mouseout(function(){
+    $("#slide5").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza6").mouseover(function(){
+    $("#slide6").show();
+  }).mouseout(function(){
+    $("#slide6").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza7").mouseover(function(){
+    $("#slide7").show();
+  }).mouseout(function(){
+    $("#slide7").hide();
+  });
+});
+$(document).ready(function(){
+  $("#pizza8").mouseover(function(){
+    $("#slide8").show();
+  }).mouseout(function(){
+    $("#slide8").hide();
+  });
+});
